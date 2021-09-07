5f20ab521465b16dae9f70cbac26b8bf36039d65
XuanTu <tuvx2000@gmail.com>
2021-09-07 16:19:34 +0700

    First commit

---
+++ app/src/main/java/com/example/xuantuandroid/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.xuantuandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void signin(View view) {
+        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+    }
+
+    public void signup(View view) {
+        startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+
+    }
+}
---
+++ app/src/main/java/com/example/xuantuandroid/RegistrationActivity.java
@@ -0,0 +1,84 @@
+package com.example.xuantuandroid;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    EditText name,email,password;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        getSupportActionBar().hide();
+
+        auth = FirebaseAuth.getInstance();
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+
+
+    }
+
+    public void signup(View view) {
+      String userName = name.getText().toString();
+      String userPassword = password.getText().toString();
+      String userEmail = email.getText().toString();
+
+
+        if(TextUtils.isEmpty(userName)){
+            Toast.makeText(this,"Enter Name!",Toast.LENGTH_LONG).show();
+            return;
+         }
+        if(TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this,"Enter PassWord!",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this,"Enter Email!",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(userPassword.length() <6 ){
+            Toast.makeText(this,"Enter PassWord more than 6 Characters!",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(RegistrationActivity.this,"Success Register", Toast.LENGTH_SHORT);
+                    startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+                }else{
+                    Toast.makeText(RegistrationActivity.this, "Register Failed" + task.getException(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        //startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+
+    }
+
+    public void signin(View view) {
+        startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+
+    }
+}
