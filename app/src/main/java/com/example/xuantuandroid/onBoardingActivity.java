8e8f87ac5043bcec15db2ba99a53515f19f279db
XuanTu <tuvx2000@gmail.com>
2021-09-10 14:16:39 +0700

    Steps 2
    Slider and Onboard

---
+++ app/src/main/java/com/example/xuantuandroid/onBoardingActivity.java
@@ -0,0 +1,14 @@
+package com.example.xuantuandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class onBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
